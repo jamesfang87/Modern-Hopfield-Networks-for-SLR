ee70085daa37c1905707c9fae2a3282cff563e4e
James Fang <james.fang.713@gmail.com>
2024-10-19 14:29:36 -0500

    fixes

--- videoreader.py
+++ videoreader.py
@@ -16,7 +16,7 @@ class VideoReader:
         """
         Reads in and returns the 3D locations of important hand and pose landmarks from a video of a sign.
 
-        :param video_path: path to the video:
+        :param video_path: path to the video
         :param show_video: whether to display the video or not
         """
         with self.mp_holistic.Holistic() as holistic:
@@ -24,7 +24,7 @@ class VideoReader:
 
             # change video_results so that it is np.zeros with shape T x num_landmarks x spatial dimensions (3D)
             total_frames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
-            self.video_results = np.zeros((int(total_frames), 21 + 15, 3))
+            self.video_results = np.zeros((int(total_frames), 21 + 21 + 15, 3))
             
             frame_num = 0
             while cap.isOpened():
@@ -37,7 +37,7 @@ class VideoReader:
                 frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
                 # generate results from holistic model
                 results = holistic.process(frame)
-                self.extract_important_landmarks(results)
+                self.extract_important_landmarks(results, frame_num)
 
                 # if show_video is true and we have results
                 if show_video and results is not None:
@@ -62,8 +62,9 @@ class VideoReader:
     def extract_important_landmarks(self, results, frame_num: int) -> None:
         """
         Extracts all hand landmarks and pose landmarks numbered 0-14 from a results object
-        the x, y, z coordinates of these landmarks are stored into self.video_results. Hand
-        landmarks are stored in the first 20 indices and pose landmarks in the next (and last) 14.
+        the x, y, z coordinates of these landmarks are stored into self.video_results. Left hand
+        landmarks are stored in the first 20 indices, right hand landmarks are stored in the next 20,
+        and pose landmarks in the last 14.
         
         Precondition: results and self.video_results is not None, 0 <= frame_num < max number of frames
 
@@ -72,19 +73,28 @@ class VideoReader:
         """
         # extract all from hands
         for i in range(21):
-            temp = results.hand_landmarks.landmark[i]
-            x, y, z = temp.x, temp.y, temp.z
-            self.video_results[frame_num, i] = np.array([x, y, z])
+            if results.left_hand_landmarks:
+                temp = results.left_hand_landmarks.landmark[i]
+                x, y, z = temp.x, temp.y, temp.z
+                self.video_results[frame_num, i] = np.array([x, y, z])
+
+        # extract all from hands
+        for i in range(21):
+            if results.right_hand_landmarks:
+                temp = results.right_hand_landmarks.landmark[i]
+                x, y, z = temp.x, temp.y, temp.z
+                self.video_results[frame_num, 20 + i] = np.array([x, y, z])
 
         # extract landmarks 0-14 from pose
         for i in range(15):
-            temp = results.pose_landmarks.landmark[i]
-            x, y, z = temp.x, temp.y, temp.z
-            self.video_results[frame_num, i + 21, 0] = np.array([x, y, z])
+            if results.pose_landmarks:
+                temp = results.pose_landmarks.landmark[i]
+                x, y, z = temp.x, temp.y, temp.z
+                self.video_results[frame_num, i + 40] = np.array([x, y, z])
 
     def draw_landmarks(self, frame, results):
         """
-        Draws results from mediapipe holistic model on the frame
+        Draws results from mediapipe holistic model on the frame and returns the new frame
         """
         self.mp_drawing.draw_landmarks(
                     frame,
@@ -112,4 +122,11 @@ class VideoReader:
         return frame
     
     def write_data(self, file_name: str, output_path: str) -> None:
+        """
+        Saves the 3D locations read from the video into output_path/file_name.npy (as a .npy file)
+
+        :param file_name: the name of the .npy file to save the data as
+        :param output_path: the path to put the .npy file into
+        :return: None
+        """
         np.save(output_path + "/" + file_name, self.video_results)
