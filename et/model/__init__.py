2108f7faf36ec60052941b3073870ef54818d819
James Fang <james.fang.713@gmail.com>
2024-11-08 12:42:23 -0600

    update to use et model

--- et/model/__init__.py
+++ et/model/__init__.py
@@ -1,4 +1,3 @@
 from .core import EnergyLayerNorm, HNN, HNN_LSE, Attention, HopfieldTransformer
 
 from .et import EnergyTransformer as GraphET
-from .et_img import EnergyTransformer as GraphImageET
